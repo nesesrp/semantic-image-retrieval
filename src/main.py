3f275e12b1e618e400aa092ed8b53a2ee3a6defe
nesesrp <nesesarpp@gmail.com>
2026-07-24 11:22:02 +0300

    chore: remove src/main.py

--- src/main.py
+++
@@ -1,9 +0,0 @@
-def get_full_name(first_name: str, last_name: str):
-    full_name = first_name.title() + " " + last_name.title()
-    return full_name
-
-print(get_full_name("john" , "doe"))
-
-def get_name_with_age(name: str, age: int):
-    name_with_age = name + "is this old: " + age
-    return name_with_age
